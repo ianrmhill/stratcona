8d089461d1921879627934b5b78627d0fcf27869
Ian Hill <ianrmhill@gmail.com>
2025-02-04 19:07:02 -0800

    WIP commit of jit testing script, added custom classes for recompiling on dimension changes

--- demos/jit_testing.py
+++ demos/jit_testing.py
@@ -7,6 +7,7 @@ import jax.numpy as jnp
 import jax.random as rand
 import jax
 
+from dataclasses import dataclass
 from functools import partial
 import timeit
 
@@ -18,30 +19,93 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import stratcona
 
 
-def main():
+@dataclass(frozen=True)
+class ExpDims:
+    name: str
+    lot: int
+    chp: int
 
+    def __init__(self, name, lot, chp):
+        object.__setattr__(self, 'name', name)
+        object.__setattr__(self, 'lot', lot)
+        object.__setattr__(self, 'chp', chp)
+
+    def __members(self):
+        return self.name, self.lot, self.chp
+
+    def __hash__(self):
+        return hash(self.__members())
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.__members() == other.__members()
+
+
+@dataclass(frozen=True)
+class TestDims():
+    name: str
+    exps: tuple[ExpDims, ...]
+
+    def __init__(self, name, config):
+        object.__setattr__(self, 'name', name)
+        object.__setattr__(self, 'exps', tuple([ExpDims(name=exp, **config[exp]) for exp in config]))
+
+    def __members(self):
+        return self.name, *self.exps
+
+    def __hash__(self):
+        return hash(self.__members)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.__members() == other.__members()
+
+
+def classes():
+    e1 = ExpDims('f', 2, 2)
+    e2 = ExpDims('g', 3, 2)
+    e3 = ExpDims('g', 3, 2)
+    print(e1 == e2)
+    print(e2 == e3)
+    t1 = TestDims('mytest', {'f': {'lot': 2, 'chp': 2}, 'g': {'lot': 3, 'chp': 2}})
+    t3 = TestDims('mytest', {'g': {'lot': 3, 'chp': 2}, 'f': {'lot': 2, 'chp': 2}})
+    t2 = TestDims('2test', {'f': {'lot': 2, 'chp': 2}})
+    print(hash(t1))
+    print(hash(t3))
+    print(t1 == t2)
+    print(t1 == t1)
+
+
+def main():
     key = rand.key(363)
     k1, k2 = rand.split(key)
 
-    def basic_model(n_dev):
-        a = npyro.sample('a', dists.Normal(0, 1))
-        b = npyro.sample('b', dists.Normal(0, 1))
-        with npyro.plate('dev', n_dev):
-            npyro.sample('y', dists.Normal(a, b))
+    tf = dists.transforms.SoftplusTransform()
 
-    def basic_sample(k, n_dev):
-        return seed(basic_model, k)(n_dev)
+    def basic_model(dims, priors, conds):
+        a = npyro.sample('a', dists.Normal(**priors['a']))
+        b = npyro.sample('b', dists.TransformedDistribution(dists.Normal(**priors['b']), tf))
+        for exp in dims.exps:
+            with npyro.plate('chp', exp.chp):
+                pre_y = npyro.deterministic('pre_y', a * conds[exp.name]['temp'])
+                npyro.sample('y', dists.Normal(pre_y, b))
 
-    to_time = partial(basic_sample, k1, 50)
+    def basic_sample(k, dims, priors, conds):
+        return trace(seed(basic_model, k)).get_trace(dims, priors, conds)['y']['value']
+
+    dims = TestDims('t1', {'e': {'lot': 1, 'chp': 50}})
+    pri = {'a': {'loc': 1, 'scale': 0.5}, 'b': {'loc': 0.5, 'scale': 0.2}}
+    c = {'e': {'temp': 3}}
+    to_time = partial(basic_sample, k1, dims, pri, c)
     best_perf = min(timeit.Timer(to_time).repeat(repeat=100, number=100))
     print(f'Best unjitted: {best_perf}')
 
     # Now jit compile it
-    j_sample = jax.jit(basic_sample, static_argnames='n_dev')
+    j_sample = jax.jit(basic_sample, static_argnames='dims')
     # Run it once with the correct static args to compile before timing
-    j_sample(k1, 500)
+    j_sample(k1, dims, pri, c)
+    d2 = TestDims('t2', {'e': {'lot': 1, 'chp': 5}})
+    print(j_sample(k1, d2, pri, c))
 
-    to_time = partial(j_sample, k1, 500)
+    to_time = partial(j_sample, k1, dims, pri, c)
     best_perf = min(timeit.Timer(to_time).repeat(repeat=100, number=100))
     print(f'Best jitted: {best_perf}')
 
